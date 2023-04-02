3e773b4d77f3fd620fa777d123a185386b6f57b2
arvind2602 <guptaarvind2602@gmail.com>
2023-04-02 23:46:24 +0530

    add all

--- blog/blog/settings.py
+++ blog/blog/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-tjgy3@ntuxwksdr&^68_bayvm33m^af=2d0y*uasc(+9d$0@b3'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
